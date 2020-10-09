30c2b0c7a4210c10277ec82468593488d718aac6
Kanishka Narayan <kanishkan91@gmail.com>
2020-10-09 19:33:58 -0400

    Updating tests
    1. Adding tests to make sure MC function can be set up correctly
    2. Adding tests to check clustering

--- tests/testthat/test_params.R
+++ tests/testthat/test_params.R
@@ -23,6 +23,16 @@ test_that('Optimization when fitting parameters is accurate', {
            New probability density is lower than old probability density.")
 })
 
+test_that('Check that clustering algorithm is generating enough clusters',{
+
+    Data <- read.csv("test_outputs/Training_Data.csv") %>% filter(year %in% c(2000,2010))
+
+    create_dataset_for_parameter_fit(data=Data,min_clusters = 20,outdir = tempdir())->tmp
+
+    expect(length(unique(tmp$clusterID))>20,"Clustering algorithm is generating skewed results. Parameters calculated may not be valid.")
+})
+
+
 
 
 test_that('Clusters created are valid ', {
@@ -33,4 +43,16 @@ test_that('Clusters created are valid ', {
     min_clusters <- 20
 
     expect(length(unique_clusters) >= min_clusters,"Clusters created in the processed data are under 20. sigma^2 values generated may be unreliable.")
+})
+
+
+test_that('MC function can be set up correctly with old data', {
+    old_data <- read.csv("test_outputs/food-dmnd-price-allrgn.csv")
+
+    expect_silent(func_MC <- mc.setup("test_outputs/food-dmnd-price-allrgn.csv"))
+    original_param_vector <- c(c(1.28,1.14,-0.19,0.21,-0.33,0.5,0.1,16,5.06,100,20))
+
+    expect_silent(original_probability_density <- func_MC(original_param_vector))
+
+    expect_equal(original_probability_density,-195,tolerance=0.01,info= paste("Unable to generate original probability density from Edmonds et al."))
 })
